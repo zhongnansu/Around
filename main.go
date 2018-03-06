cdeef9f54cf59b5b42de969e22fc71e5dcdbca7a
zhongnan.su@outlook.com <zhongnan.su@outlook.com>
2018-03-05 23:08:32 -0500

    add app.yaml

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.231.26.185:9200"
+	ES_URL = "http://35.196.193.207:9200"
 )
 
 
